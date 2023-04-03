d3255a5d2d6a103354357628e3bef983da397af2
Jondiii <jondi@opendeusto.es>
2023-04-03 18:56:05 +0200

    deberia ir pero no va

--- Proyecto/generadorKeys.c
+++ Proyecto/generadorKeys.c
@@ -60,7 +60,7 @@ void next_candidate2(char *candidate, char *alpha)
       if (nosPasamos) {
         candidate[i] = alpha[pos + 1]; //Esto tiene que hacer +1 cuando no estemos en la última posición
         done = 1;
-        break;  
+        break;
       }
       candidate[i] = alpha[pos + NTHREADS]; //Esto tiene que hacer +1 cuando no estemos en la última posición
       done = 1;
@@ -77,6 +77,12 @@ void next_candidate2(char *candidate, char *alpha)
   }
 }
 
+void init_candidates(char *candidates, char *alpha) {
+  for (int i = 0; i < NTHREADS; i++) {
+   sprintf(&candidates[i * (CHUNK/NTHREADS)], "%c", alpha[i]);
+  }
+}
+
 
 int main(int argc, char **argv)
 {   
@@ -85,28 +91,48 @@ int main(int argc, char **argv)
     
     avalue = (char *)malloc(sizeof(char) * strlen(ALPHA));
     sprintf(avalue, "%s", ALPHA);
-    
+  
     int m = 6;
     char secreto[HASHSIZE + 1];
     char hashString[HASHSIZE + 1];
     char candidate[MAXCANDIDATE];
     char candidates[CHUNK][m + 1];
 
-    int threadIDs[CHUNK];
-    sprintf(candidate, "%c", avalue[0]);
+    int firstTime = 1;
 
-    
-    //pragma omp parallel for private(hashString) shared(candidate)
-    #pragma omp parallel for private(candidate)
-    for (int i = 0; i < CHUNK; i++) {
-        sprintf(candidates[i], "%s", candidate);
-        next_candidate2(candidate, ALPHA);  //HACER QUE EN VEZ DE next (+1) sea +N_procesos.
-                                            //Inicializar un array de candidatas que tengan también estos saltos.
-        threadIDs[i] = omp_get_thread_num();
-        }
-    
-    for (int i = 0; i < CHUNK; i++) {
-        printf("T%d-Combinación %d: %s\n", threadIDs[i], i, candidates[i]);
+    int threadIDs[CHUNK];
+    sprintf(candidates[0], "%c", avalue[0]);
+    sprintf(candidates[CHUNK/NTHREADS], "%c", avalue[0]);
+
+    for (int i = 0; i < NTHREADS; i++){
+      sprintf(candidates[(CHUNK/NTHREADS) * i], "%c", avalue[i]);
+      
+      printf("%c\n",avalue[i]);
+      printf("%s\n",candidates[(CHUNK/NTHREADS) * i]);
+      printf("%d\n",(CHUNK/NTHREADS) * i);
+    }
+
+    // gcc -fopenmp generadorKeys.c -o 2generadorKeys
+    for (int j = 0; j <= 1; j++) {
+      //#pragma omp parallel for private(hashString) shared(candidate)
+      #pragma omp parallel for private(candidate, firstTime)
+      for (int i = 0; i < CHUNK; i++) {
+          if (firstTime) {
+            sprintf(candidate, "%s", candidates[(CHUNK/NTHREADS) * omp_get_thread_num()]);
+            printf("%d\n",(CHUNK/NTHREADS) * omp_get_thread_num());
+            firstTime = 0;
+          }
+
+          sprintf(candidates[i], "%s", candidate);
+          next_candidate2(candidate, ALPHA);  //HACER QUE EN VEZ DE next (+1) sea +N_procesos.
+                                              //Inicializar un array de candidatas que tengan también estos saltos.
+          threadIDs[i] = omp_get_thread_num();
+          }
+      
+      for (int i = 0; i < CHUNK; i++) {
+          printf("T%d-Combinación %d: %s\n", threadIDs[i], i, candidates[i]);
+      }
+      printf("\n");
     }
   return 0;
 }
